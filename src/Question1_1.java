9380ca9fee52ddd966dd7ff8827edd9ac15992e2
Léo Gouttefarde <leo.gouttefarde@ensimag.grenoble-inp.fr>
2016-12-08 11:02:09 +0100

    init

---
+++ src/Question1_1.java
@@ -0,0 +1,98 @@
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
+
+import java.io.IOException;
+
+public class Question1_1 {
+
+	// type clé input, type valeur input, type clé output, type valeur output
+	public static class WordCountMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+		@Override
+		protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+			for (String word : value.toString().split("\\s+")) {
+				context.write(new Text(word), new IntWritable(1));
+			}
+		}
+	}
+
+	// type clé input, type valeur input, type clé output, type valeur output
+	public static class WordCountReducer extends Reducer<Text, IntWritable, Text, LongWritable> {
+		@Override
+		protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+			long sum = 0;
+			for (IntWritable value : values) {
+				sum += value.get();
+			}
+			context.write(key, new LongWritable(sum));
+		}
+	}
+
+//	public static class MyMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+//		@Override
+//		protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+//
+//		}
+//	}
+//
+//	public static class MyReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+//		@Override
+//		protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+//
+//		}
+//	}
+
+	public static void main(String[] args) throws Exception {
+		Configuration conf = new Configuration();
+		String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+
+		String input;
+		String output;
+
+		if (otherArgs.length < 2) {
+			output = "sortie";
+
+			if (otherArgs.length < 1) {
+				input = "texte.txt";
+			}
+            else {
+                input = otherArgs[0];
+            }
+		}
+        else {
+            output = otherArgs[1];
+            input = otherArgs[0];
+        }
+		
+		Job job = Job.getInstance(conf, "Question1_1");
+		job.setJarByClass(Question1_1.class);
+		
+		job.setMapperClass(WordCountMapper.class);
+		job.setMapOutputKeyClass(Text.class);
+		job.setMapOutputValueClass(IntWritable.class);
+
+		job.setReducerClass(WordCountReducer.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+
+        //job.setCombinerClass(WordCountCombiner.class);
+		
+		FileInputFormat.addInputPath(job, new Path(input));
+		job.setInputFormatClass(TextInputFormat.class);
+		
+		FileOutputFormat.setOutputPath(job, new Path(output));
+		job.setOutputFormatClass(TextOutputFormat.class);
+		
+		System.exit(job.waitForCompletion(true) ? 0 : 1);
+	}
+}
